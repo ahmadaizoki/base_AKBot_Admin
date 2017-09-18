d34d4297b4520d60c67c53fa8de48b0d8ba367d8
ahmadaizoki <ahmadaizoki@githup.com>
2017-09-18 11:57:14 +0200

    base

---
+++ templates/index.php
@@ -0,0 +1,106 @@
+<html>
+<title>
+  Base Admin
+</title>
+<header>
+  <h1>Base Admin</h1>
+  <br>
+  <br>
+</header>
+<body>
+  <form method="post" action="/">
+    Nombre de jours:<br>
+    <input type="text" name="nbdays">
+    <br>
+    Nombre max de nuits:<br>
+    <input type="text" name="nbmax">
+    <br>
+    Nombre min de nuits:<br>
+    <input type="text" name="nbmin">
+    <br>
+    Nom de jour d'arriver:<br>
+    <select name="dayn">
+      <option value="lundi">Lundi</option>
+      <option value="mardi">Mardi</option>
+      <option value="mercredi">Mercredi</option>
+      <option value="jeudi">Jeudi</option>
+      <option value="vendredi">Vendredi</option>
+      <option value="samedi">Samedi</option>
+      <option value="dimanche">Dimanche</option>
+    </select>
+    <br>
+    Nom de jour de depart:<br>
+    <select name="dayt">
+      <option value="lundi">Lundi</option>
+      <option value="mardi">Mardi</option>
+      <option value="mercredi">Mercredi</option>
+      <option value="jeudi">Jeudi</option>
+      <option value="vendredi">Vendredi</option>
+      <option value="samedi">Samedi</option>
+      <option value="dimanche">Dimanche</option>
+    </select>
+    <br>
+    Nom d'offre:<br>
+    <input type="text" name="nom">
+    <br>
+    Rate:<br>
+    <input type="text" name="rate">
+    <br>
+    Choisir une offre:<br>
+    <select name="offre">
+      <option value="1">Offre 1</option>
+      <option value="2">Offre 2</option>
+      <option value="3">Offre 3</option>
+    </select>
+    <br>
+    <br>
+    <input type="submit">
+  </form>
+  <?php
+    $pdoString = "pgsql:host=ec2-54-75-224-100.eu-west-1.compute.amazonaws.com dbname=deoaedt1t45duq user=ufmqwqytarffyx password=96a05ed81622bac458a19cff32a97e37fb2ae74bfb2a1e3f3b484fdf30735b82";
+    $pdo = new PDO($pdoString);
+    if (!$pdo) {
+      die("Could not connect");
+    }
+    $sql = "select * from offre";
+    $statement = $pdo->query($sql);
+    $display = "    <table border=\"1\">\n";
+    while (($row = $statement->fetch(PDO::FETCH_ASSOC))) {
+      $display .= "      <tr>\n";
+      $display .= "        <td>" . $row["id"] . "</td>\n";
+      $display .= "        <td>" . $row["nbdays"] . "</td>\n";
+      $display .= "        <td>" . $row["nbmax"] . "</td>\n";
+      $display .= "        <td>" . $row["nbmin"] . "</td>\n";
+      $display .= "        <td>" . $row["dayn"] . "</td>\n";
+      $display .= "        <td>" . $row["dayt"] . "</td>\n";
+      $display .= "        <td>" . $row["nom"] . "</td>\n";
+      $display .= "        <td>" . $row["rate"] . "</td>\n";
+      $display .= "      </tr>\n";
+    }
+    echo $display;
+  ?>
+</body>
+</html>
+<?php
+  $pdoString = "pgsql:host=ec2-54-75-224-100.eu-west-1.compute.amazonaws.com dbname=deoaedt1t45duq user=ufmqwqytarffyx password=96a05ed81622bac458a19cff32a97e37fb2ae74bfb2a1e3f3b484fdf30735b82";
+  $pdo = new PDO($pdoString);
+  if (!$pdo) {
+    die("Could not connect");
+  }
+  $sql = "select * from offre";
+  $statement = $pdo->query($sql);
+  $display = "    <table border=\"1\">\n";
+  while (($row = $statement->fetch(PDO::FETCH_ASSOC))) {
+    $display .= "      <tr>\n";
+    $display .= "        <td>" . $row["id"] . "</td>\n";
+    $display .= "        <td>" . $row["nbdays"] . "</td>\n";
+    $display .= "        <td>" . $row["nbmax"] . "</td>\n";
+    $display .= "        <td>" . $row["nbmin"] . "</td>\n";
+    $display .= "        <td>" . $row["dayn"] . "</td>\n";
+    $display .= "        <td>" . $row["dayt"] . "</td>\n";
+    $display .= "        <td>" . $row["nom"] . "</td>\n";
+    $display .= "        <td>" . $row["rate"] . "</td>\n";
+    $display .= "      </tr>\n";
+  }
+  echo $display;
+?>
