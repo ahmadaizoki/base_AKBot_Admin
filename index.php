0b6a4c67faed31f94ccfee18e214a6b936298107
ahmadaizoki <ahmadaizoki@githup.com>
2017-09-19 16:27:34 +0200

    base

--- index.php
+++ index.php
@@ -89,6 +89,7 @@
         $qu="SELECT * FROM offre WHERE id=1";
         $dbc=pg_connect("host=ec2-54-75-224-100.eu-west-1.compute.amazonaws.com port=5432 dbname=deoaedt1t45duq user=ufmqwqytarffyx password=96a05ed81622bac458a19cff32a97e37fb2ae74bfb2a1e3f3b484fdf30735b82");
         $results=pg_query($dbc,$qu);
+        $res=$results->FETCH_ASSOC;
         //echo $result;
         $display .= "        <td> <a href=update.php?" . $results .">Edit</a> </td>\n";
         //$display .= "        <td> <a href=update.php?id=" . $result["id"] . "&nbdays=" . $result["nbdays"] . "&nbmax=" . $result["nbmax"] . "&nbmin=" . $result["nbmin"] . "&dayn=" . $result["dayn"] . "&dayt=" . $result["dayt"] . "&nom=" . $result["nom"] . "&rate=" . $result["rate"] . ">Edit</a> </td>\n";
